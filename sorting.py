861c3aff74a25b14404bf4a3ef1439ef98c13a95
Tanicka <283190@vutbr.cz>
2026-04-20 12:41:12 +0200

    ukol1

---
+++ sorting.py
@@ -0,0 +1,22 @@
+#ukol1
+import random
+
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
+
+def selection_sort(seznam):
+    seznam = seznam[:]
+    for pozice_ukladani in range(len(seznam)):
+        min_idx = pozice_ukladani
+        for pozice_prochazena in range(pozice_ukladani + 1, len(seznam)):
+            if seznam[pozice_prochazena] < seznam[min_idx]:
+                min_idx=pozice_prochazena
+        seznam[pozice_ukladani], seznam[min_idx] = seznam[min_idx], seznam[pozice_ukladani]
+    return seznam
+
+
+seznam = [5, 1, 4, 2, 8]
+print(f"Puvodni seznam:" , seznam)
+print(f"Serazeny seznam:", selection_sort(seznam))
+
+#ukol2
